4c310462acab1e20b419aa7140dcffc3aaf4fca7
im miolab <poson_hemp@yahoo.co.jp>
2020-02-08 20:44:33 +0900

    apply class Url

--- app.py
+++ app.py
@@ -1,3 +1,23 @@
+""" Tag Information
+- Qiita
+    <h1 class="searchResult_itemTitle">
+        <a href="/garakutayama/items/0768bc6135ec0683df67">article_title</a>
+    </h1>
+
+- Stack Over Flow
+    <div class="result-link">
+        <h3>
+            <a href="/questions/53660779/including-node-package-into-python-aws-lambda?r=SearchResults" data-searchsession="/questions/53660779/including-node-package-into-python-aws-lambda?r=SearchResults&amp;s=15|109.3204" title="Including node package into Python AWS Lambda" class="question-hyperlink">
+            Q: Including node package into Python AWS Lambda        </a>
+        </h3>
+    </div>
+
+- Developers.IO
+    <p class="title">
+        <a href="https://dev.classmethod.jp/etc/ec2-image-builder-codepipeline/">設定ファイルPushでゴールデンイメージを自動作成する構成を考えてみた</a>
+    </p>
+"""
+
 import sys
 import os
 import re
@@ -8,6 +28,9 @@ from flask import Flask, render_template, request, session
 import requests
 from bs4 import BeautifulSoup as bs
 # from wtforms import Form, TextAreaField, validators
+
+import pprint
+import pytest
 
 
 app = Flask(__name__)
@@ -23,79 +46,61 @@ def index():
 def result():
     input_text = request.form["input_text"]
     input_text.strip()
+
     if re.search("\s", input_text):
         word_in_url = re.sub(r"\s+", "+", input_text)
     else:
         word_in_url = input_text
 
-    # URL
+    class Url:
+        def __init__(self, url, tag, css_class):
+            self.url = url
+            self.tag = tag
+            self.css_class = css_class
+
+        def fn_test(self):
+            html_req = requests.get(self.url)
+            html_contents = html_req.content
+            soup = bs(html_contents, 'html.parser')
+            self.title_list = soup.find_all(self.tag, class_=self.css_class)
+            return self.title_list
+
+
     # // Qiita
-    url_qiita = "https://qiita.com/search?q=" + word_in_url + "&sort=rel"
+    url_qiita = Url(
+        url = "https://qiita.com/search?q=" + word_in_url + "&sort=rel",
+        tag = "h1",
+        css_class = "searchResult_itemTitle",
+        # title_list = []
+    )
+    url_qiita.fn_test()
+
+
+    # print(url_qiita.url)
+    pprint.pprint(url_qiita.title_list)
+
+
     # // Stack Over Flow
-    url_sof = "https://stackoverflow.com/search?q=" + word_in_url
+    url_sof = Url(
+        url = "https://stackoverflow.com/search?q=" + word_in_url,
+        tag = "div",
+        css_class = "result-link"
+    )
+
     # // Developers.IO
-    url_dev = "https://dev.classmethod.jp/?s=" + word_in_url
-
-    """ Tag Information
-    - Qiita
-        <h1 class="searchResult_itemTitle">
-            <a href="/garakutayama/items/0768bc6135ec0683df67">article_title</a>
-        </h1>
-
-    - Stack Over Flow
-        <div class="result-link">
-            <h3>
-                <a href="/questions/53660779/including-node-package-into-python-aws-lambda?r=SearchResults" data-searchsession="/questions/53660779/including-node-package-into-python-aws-lambda?r=SearchResults&amp;s=15|109.3204" title="Including node package into Python AWS Lambda" class="question-hyperlink">
-                Q: Including node package into Python AWS Lambda        </a>
-            </h3>
-        </div>
-
-    - Developers.IO
-        <p class="title">
-            <a href="https://dev.classmethod.jp/etc/ec2-image-builder-codepipeline/">設定ファイルPushでゴールデンイメージを自動作成する構成を考えてみた</a>
-        </p>
-    """
-
-    dict_qiita = {
-        "url": url_qiita,
-        "tag": "h1",
-        "css_class": "searchResult_itemTitle"
-    }
-    dict_sof = {
-        "url": url_sof,
-        "tag": "div",
-        "css_class": "result-link"
-    }
-    dict_dev = {
-        "url": url_dev,
-        "tag": "p",
-        "css_class": "title"
-    }
-
-    # print(dict_dev["url"])
+    url_dev = Url(
+        url = "https://dev.classmethod.jp/?s=" + word_in_url,
+        tag = "p",
+        css_class = "title"
+    )
 
 
 
 
     # // ON GOING //
 
-    # def fn_(url, tag, css_class):
-        
-    #     html_req = requests.get(url)
-    #     html_contents = html_req.content
-
-    #     # // Set Parser
-    #     soup = bs(html_contents, 'html.parser')
-
-        # css_titles_h1_class = "searchResult_itemTitle"
-        # title_list = soup.find_all("h1", class_=css_titles_h1_class)
-
-
-
-
     # for i in range(title_list):
     #     time.sleep(0.1)
-    #     print(title_list[i].text)
 
 
 
