ddc5b915ee0dc9aae34e43c3cc4c6ec93171aeb8
im miolab <poson_hemp@yahoo.co.jp>
2020-02-08 16:22:16 +0900

    add url(sof, dev.IO) and each dictionary

--- app.py
+++ app.py
@@ -28,26 +28,76 @@ def result():
     else:
         word_in_url = input_text
 
-    # // "関連順"
-    url = "https://qiita.com/search?q=" + word_in_url + "&sort=rel"
-    # // "新着順"
-    # url = "https://qiita.com/search?q=" + word_in_url + "&sort=created"
-    print(url)
+    # URL
+    # // Qiita
+    url_qiita = "https://qiita.com/search?q=" + word_in_url + "&sort=rel"
+    # // Stack Over Flow
+    url_sof = "https://stackoverflow.com/search?q=" + word_in_url
+    # // Developers.IO
+    url_dev = "https://dev.classmethod.jp/?s=" + word_in_url
 
-    html_req = requests.get(url)
-    html_contents = html_req.content
+    """ Tag Information
+    - Qiita
+        <h1 class="searchResult_itemTitle">
+            <a href="/garakutayama/items/0768bc6135ec0683df67">article_title</a>
+        </h1>
+
+    - Stack Over Flow
+        <div class="result-link">
+            <h3>
+                <a href="/questions/53660779/including-node-package-into-python-aws-lambda?r=SearchResults" data-searchsession="/questions/53660779/including-node-package-into-python-aws-lambda?r=SearchResults&amp;s=15|109.3204" title="Including node package into Python AWS Lambda" class="question-hyperlink">
+                Q: Including node package into Python AWS Lambda        </a>
+            </h3>
+        </div>
+
+    - Developers.IO
+        <p class="title">
+            <a href="https://dev.classmethod.jp/etc/ec2-image-builder-codepipeline/">設定ファイルPushでゴールデンイメージを自動作成する構成を考えてみた</a>
+        </p>
+    """
+
+    dict_qiita = {
+        "url": url_qiita,
+        "tag": "h1",
+        "css_class": "searchResult_itemTitle"
+    }
+    dict_sof = {
+        "url": url_sof,
+        "tag": "div",
+        "css_class": "result-link"
+    }
+    dict_dev = {
+        "url": url_dev,
+        "tag": "p",
+        "css_class": "title"
+    }
+
+    # print(dict_dev["url"])
 
 
-    # // Set Parser
-    soup = bs(html_contents, 'html.parser')
 
-    # // Tag Information
-    # <h1 class="searchResult_itemTitle"><a href="/garakutayama/items/0768bc6135ec0683df67">【<em>AWS</em>勉強】<em>AWS</em> SDK　概要</a></h1>
 
-    css_titles_h1_class = "searchResult_itemTitle"
-    title_list = soup.find_all("h1", class_=css_titles_h1_class)
+    # // ON GOING //
 
-    print(title_list[0].text)
+    # def fn_(url, tag, css_class):
+        
+    #     html_req = requests.get(url)
+    #     html_contents = html_req.content
+
+    #     # // Set Parser
+    #     soup = bs(html_contents, 'html.parser')
+
+        # css_titles_h1_class = "searchResult_itemTitle"
+        # title_list = soup.find_all("h1", class_=css_titles_h1_class)
+
+
+
+
+    # for i in range(title_list):
+    #     time.sleep(0.1)
+    #     print(title_list[i].text)
+
+
 
 
     # return render_template('index.html', title_list)
